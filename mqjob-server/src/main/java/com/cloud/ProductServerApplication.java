1d0b1c22c203df282534e27f4ff4d3c826c45e50
zhoushuai <@Zs195310>
2022-02-22 21:05:19 +0800

    添加mqjob

---
+++ mqjob-server/src/main/java/com/cloud/ProductServerApplication.java
@@ -0,0 +1,33 @@
+package com.cloud;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication(exclude = {DruidDataSourceAutoConfigure.class, DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableAsync
+@RefreshScope
+public class ProductServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServerApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
+
