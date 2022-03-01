70865c3cf59c55b969f844044316ee48b2066313
zhoushuai <@Zs195310>
2022-03-01 20:03:49 +0800

    集成camunda工作流引擎

---
+++ camunda-server/src/main/java/com/cloud/config/WebMvcConfig.java
@@ -0,0 +1,80 @@
+package com.cloud.config;
+
+import com.cloud.common.beans.Factory.StringToEnumConverterFactory;
+import com.cloud.common.interceptor.LoginInterceptor;
+import com.cloud.common.utils.JsonUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.*;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author： Zhou Shuai
+ * @Date： 9:08 2018/12/19
+ * @Description：
+ * @Version:  01
+ */
+@Configuration
+@Slf4j
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        log.debug("进入LoginHandlerFilter拦截器");
+        InterceptorRegistration ir = registry.addInterceptor(loginInterceptor);
+        //配置拦截的url
+        ir.addPathPatterns("/**");
+        //配置不拦截的路径
+        ir.excludePathPatterns("/static/**")
+        .excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 设置允许跨域的路径
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                // 是否允许证书 不再默认开启
+                .allowCredentials(true)
+                // 设置允许的方法
+                .allowedMethods("*")
+                // 跨域允许时间
+                .maxAge(3600);
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverterFactory(new StringToEnumConverterFactory());
+    }
+
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter(){
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        jsonConverter.setObjectMapper(JsonUtil.OBJECT_MAPPER);
+        return jsonConverter;
+    }
+
+}
