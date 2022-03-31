e189d4d3ce1754206b610777b5200c68cda6e657
zhoushuai <zs11195310@163.com>
2022-03-31 12:11:08 +0800

    移除rocketmq模块

--- rocketmq-server/src/main/java/com/cloud/core/RocketMQListenerContainer.java
+++
@@ -1,8 +0,0 @@
-package com.cloud.core;
-
-import org.springframework.beans.factory.DisposableBean;
-
-public interface RocketMQListenerContainer extends DisposableBean {
-
-    void setupMessageListener(CloudMQListener<?> messageListener);
-}
