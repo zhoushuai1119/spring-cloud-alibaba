363b7a7ed3d5bed23111bf957af39115708647a8
zhoushuai <shuai.zhou@avatr.com>
2022-06-22 15:15:34 +0800

    List拷贝测试

--- order-server/src/test/java/com/cloud/order/OrderServerApplicationTest.java
+++ order-server/src/test/java/com/cloud/order/OrderServerApplicationTest.java
@@ -1,13 +1,17 @@
 package com.cloud.order;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.convert.Convert;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
-import com.cloud.order.mapper.elasticsearch.CategoryElasticRepository;
-import com.cloud.order.mapper.mongodb.TestMongoRepository;
 import com.cloud.order.domain.dto.MongoTestDTO;
+import com.cloud.order.domain.dto.MongoTestV2DTO;
 import com.cloud.order.domain.entity.Category;
 import com.cloud.order.enums.CategoryTypeEnum;
+import com.cloud.order.mapper.elasticsearch.CategoryElasticRepository;
+import com.cloud.order.mapper.mongodb.TestMongoRepository;
 import com.cloud.order.service.CategoryService;
 import com.github.dozermapper.core.Mapper;
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.junit.Test;
@@ -22,6 +26,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 
 
 @SpringBootTest
@@ -73,7 +78,29 @@ public class OrderServerApplicationTest {
         mongoTestDTO.setTime("2021-12-31");
         mongoTestDTO.setLocalDate(new Date());
         mongoTestDTO.setLocalDateTime(LocalDateTime.now());
-        //testMongoRepository.save(mongoTestDTO);
+        testMongoRepository.save(mongoTestDTO);
+    }
+
+
+    @Test
+    public void testListCopy() {
+        List<MongoTestDTO> list = Lists.newArrayList();
+        MongoTestDTO mongoTestDTO = new MongoTestDTO();
+        mongoTestDTO.setId(6266L);
+        mongoTestDTO.setName("test");
+        mongoTestDTO.setAge(30);
+        mongoTestDTO.setAddress("上海市青浦区");
+        mongoTestDTO.setTime("2021-12-31");
+        mongoTestDTO.setLocalDate(new Date());
+        mongoTestDTO.setLocalDateTime(LocalDateTime.now());
+        list.add(mongoTestDTO);
+
+        List<MongoTestV2DTO> list2 = BeanUtil.copyToList(list,MongoTestV2DTO.class);
+        List<MongoTestV2DTO> list3 = Convert.toList(MongoTestV2DTO.class,list);
+        System.out.println(list2);
+        System.out.println(list3);
+        System.out.println(list2 == list3);
+        System.out.println(list2.equals(list3));
     }
 
 
