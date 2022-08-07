9eef91a47fa2d0bd1b3921f6fd9cbce1b5f44cf9
zhoushuai <shuai.zhou@avatr.com>
2022-08-07 09:45:59 +0800

    test reset

---
+++ common-server/src/main/java/com/cloud/common/test.java
@@ -0,0 +1,13 @@
+package com.cloud.common;
+
+/**
+ * @description:
+ * @author: zhou shuai
+ * @date: 2022/8/7 9:45
+ * @version: v1
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
