06cef20dafdaa4062dc08c7815bfde9a83dbb69e
zhoushuai <shuai.zhou@avatr.com>
2022-09-26 17:43:54 +0800

    移除mongodb和es依赖

--- order-server/src/main/java/com/cloud/order/domain/entity/Category.java
+++ order-server/src/main/java/com/cloud/order/domain/entity/Category.java
@@ -7,51 +7,39 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Data
 @TableName("category")
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(indexName = "idx_category")
 public class Category extends Model<Category> {
 
     private static final long serialVersionUID = 9072858780333387154L;
 
     @TableId(type = IdType.AUTO)
-    @Field(type = FieldType.Keyword)
     private Long id;
 
     @TableField("category_id")
-    @Field
     private String categoryId;
 
     @TableField("category_name")
-    @Field(analyzer = "ik_smart")
     private String categoryName;
 
     @TableField("category_level")
-    @Field(type = FieldType.Integer)
     private Integer categoryLevel;
 
     @TableField("parent_category_id")
-    @Field
     private String parentCategoryId;
 
     @TableField("parent_category_name")
-    @Field(analyzer = "ik_smart")
     private String parentCategoryName;
 
 
     @TableField("category_type")
-    @Field(type = FieldType.Integer)
     private CategoryTypeEnum categoryType;
 
     @Version //乐观锁版本号配置
     @TableField(value = "version")
-    @Field(type = FieldType.Integer)
     private Integer version;
 
     /**
@@ -59,13 +47,11 @@ public class Category extends Model<Category> {
      */
     @TableField("tenant_id")
     @JsonIgnore //不显示某个字段给前端
-    @Field(type = FieldType.Keyword)
     private String tenantId;
 
 
     @TableLogic //标注为逻辑删除字段
     @JsonIgnore //不显示某个字段给前端
-    @Field(type = FieldType.Integer)
     private Boolean isDeleted;
 
 }
--- order-server/src/test/java/com/cloud/order/OrderServerApplicationTest.java
+++ order-server/src/test/java/com/cloud/order/OrderServerApplicationTest.java
@@ -1,19 +1,9 @@
 package com.cloud.order;
 
-import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.core.convert.Convert;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
-import com.cloud.order.domain.dto.MongoTestDTO;
-import com.cloud.order.domain.dto.MongoTestV2DTO;
-import com.cloud.order.domain.entity.Category;
-import com.cloud.order.enums.CategoryTypeEnum;
-import com.cloud.order.mapper.elasticsearch.CategoryElasticRepository;
-import com.cloud.order.mapper.mongodb.TestMongoRepository;
 import com.cloud.order.service.CategoryService;
 import com.github.dozermapper.core.Mapper;
-import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.client.RestHighLevelClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,9 +14,6 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -38,15 +25,6 @@ public class OrderServerApplicationTest {
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
-
-    @Autowired
-    private RestHighLevelClient restHighLevelClient;
-
-    @Autowired
-    private CategoryElasticRepository categoryElasticRepository;
-
-    @Autowired
-    private TestMongoRepository testMongoRepository;
 
     @Autowired
     private CategoryService categoryService;
@@ -69,54 +47,6 @@ public class OrderServerApplicationTest {
         System.out.println(version1);
     }
 
-
-    @Test
-    public void testMongoSave() {
-        MongoTestDTO mongoTestDTO = new MongoTestDTO();
-        mongoTestDTO.setId(6266L);
-        mongoTestDTO.setName("test");
-        mongoTestDTO.setAge(30);
-        mongoTestDTO.setAddress("上海市青浦区");
-        mongoTestDTO.setTime("2021-12-31");
-        mongoTestDTO.setLocalDate(new Date());
-        mongoTestDTO.setLocalDateTime(LocalDateTime.now());
-        testMongoRepository.save(mongoTestDTO);
-    }
-
-
-    @Test
-    public void testListCopy() {
-        List<MongoTestDTO> list = Lists.newArrayList();
-        MongoTestDTO mongoTestDTO = new MongoTestDTO();
-        mongoTestDTO.setId(6266L);
-        mongoTestDTO.setName("test");
-        mongoTestDTO.setAge(30);
-        mongoTestDTO.setAddress("上海市青浦区");
-        mongoTestDTO.setTime("2021-12-31");
-        mongoTestDTO.setLocalDate(new Date());
-        mongoTestDTO.setLocalDateTime(LocalDateTime.now());
-        list.add(mongoTestDTO);
-
-        List<MongoTestV2DTO> list2 = BeanUtil.copyToList(list, MongoTestV2DTO.class);
-        List<MongoTestV2DTO> list3 = Convert.toList(MongoTestV2DTO.class, list);
-        System.out.println(list2);
-        System.out.println(list3);
-        System.out.println(list2 == list3);
-        System.out.println(list2.equals(list3));
-    }
-
-
-    @Test
-    public void saveCategoryEs() {
-        Category category = new Category();
-        category.setCategoryId("2dd");
-        category.setCategoryName("2ss");
-        category.setCategoryLevel(0);
-        category.setParentCategoryId("11");
-        category.setParentCategoryName("2dd3");
-        category.setCategoryType(CategoryTypeEnum.ORDINARY_ARCHIVE);
-        categoryElasticRepository.save(category);
-    }
 
     @Test
     public void updateTableTest() {
