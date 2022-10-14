2bcdc7eb6cea83c575c9ba735c609c5e194fe139
zhoushuai <shuai.zhou@avatr.com>
2022-10-14 16:04:17 +0800

    gateway 配置修改

--- gateway-server/src/main/java/com/cloud/gateway/controller/FallbackController.java
+++ gateway-server/src/main/java/com/cloud/gateway/controller/FallbackController.java
@@ -1,10 +1,10 @@
 package com.cloud.gateway.controller;
 
+import com.cloud.platform.common.domain.response.BaseResponse;
+import com.cloud.platform.common.enums.BaseErrorCodeEnum;
+import com.cloud.platform.common.exception.BaseException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @description: 默认降级处理
@@ -16,13 +16,11 @@ import java.util.Map;
 public class FallbackController {
 
     @RequestMapping("fallback")
-    public Map<String,String> fallback(){
-        System.out.println("降级操作...");
-        Map<String,String> map = new HashMap<>();
-        map.put("resultCode","fail");
-        map.put("resultMessage","服务异常");
-        map.put("resultObj","null");
-        return map;
+    public BaseResponse fallback(){
+        return BaseResponse.createFailResult(
+                new BaseException(BaseErrorCodeEnum.REQUEST_FAIL_FALLBACK.getCode(),
+                        BaseErrorCodeEnum.REQUEST_FAIL_FALLBACK.getMessage(), null
+                ));
     }
 
 }
