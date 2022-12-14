5db0d0d3858a92cb0bab3855b53d5c06eec534ce
zhoushuai <shuai.zhou@avatr.com>
2022-12-14 16:20:24 +0800

    netty代码修改

--- payment-server/src/main/java/com/cloud/payment/service/NettySendServiceImpl.java
+++ payment-server/src/main/java/com/cloud/payment/service/NettySendServiceImpl.java
@@ -2,7 +2,7 @@ package com.cloud.payment.service;
 
 import com.cloud.payment.netty.client.NettyClientHandler;
 import com.cloud.common.netty.NettySendService;
-import com.cloud.platform.web.netty.protocol.SmartProtocol;
+import com.cloud.platform.web.netty.protocol.MessageProtocol;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelId;
 import lombok.extern.slf4j.Slf4j;
@@ -22,7 +22,7 @@ public class NettySendServiceImpl implements NettySendService<String> {
     @Override
     public void sendMessage(String content) {
         ConcurrentHashMap<ChannelId, ChannelHandlerContext> map = NettyClientHandler.CLIENT_MAP;
-        SmartProtocol response = new SmartProtocol(content.getBytes().length, content.getBytes());
+        MessageProtocol response = new MessageProtocol(content.getBytes().length, content.getBytes());
         if (!map.isEmpty()) {
             map.forEach((k, v) -> {
                 if (v == null) {
